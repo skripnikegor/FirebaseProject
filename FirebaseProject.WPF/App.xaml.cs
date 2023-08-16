8152c808755f6f4cfc82a750deb088a58551e5ea
Egor <34675756+skripnikegor@users.noreply.github.com>
2023-08-16 21:54:46 +0300

    Added startup setting

---
+++ FirebaseProject.WPF/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Firebase.Auth;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Windows;
+
+namespace FirebaseProject.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private readonly IHost _host;
+
+        public App()
+        {
+            _host = Host
+                .CreateDefaultBuilder()
+                .ConfigureServices((context, service) =>
+                {
+                    string firebaseApiKey = context.Configuration.GetValue<string>("FIREBASE_API_KEY");
+
+                    service.AddSingleton(new FirebaseAuthProvider(new FirebaseConfig(firebaseApiKey)));
+                    service.AddSingleton<MainWindow>((services) => new MainWindow());
+                })
+                .Build();
+        }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = _host.Services.GetRequiredService<MainWindow>();
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
+    }
+}
