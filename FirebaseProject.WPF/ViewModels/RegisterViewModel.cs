cf60f379b8ce56a85d42f611a71ad562a5b42300
Egor <34675756+skripnikegor@users.noreply.github.com>
2023-08-20 09:35:39 +0300

    Added RegisterView properties

---
+++ FirebaseProject.WPF/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,42 @@
+﻿using MVVMEssentials.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirebaseProject.WPF.ViewModels
+{
+    class RegisterViewModel : ViewModelBase
+    {
+		private string _email;
+
+		public string Email
+		{
+			get
+			{
+                return _email;
+            }
+			set
+			{
+                _email = value;
+				OnPropertyChanged(nameof(Email));
+            }
+		}
+
+        private string _username;
+
+        public string Username
+        {
+            get
+            {
+                return _username;
+            }
+            set
+            {
+                _username = value;
+                OnPropertyChanged(nameof(Username));
+            }
+        }
+    }
+}
